08acd9763ca50eb8ed02d78c6697a0d42e136a1e
Parth nagar <parthnagar2345@gmail.com>
2024-09-17 20:29:56 +0530

    changes in 14 project script

--- 14. page scroll indicator/script.js
+++ 14. page scroll indicator/script.js
@@ -1,9 +1,5 @@
 let indicator=document.querySelector(".scroll-indicator .progress")
 let scroll_height=document.documentElement.scrollHeight - document.documentElement.clientHeight
-console.log(scroll_height);
-console.log(document.documentElement.scrollHeight);
-console.log(document.documentElement.clientHeight);
-
 
 window.addEventListener("scroll",scroll)
 function scroll(){
